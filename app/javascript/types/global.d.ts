4067f4e48999fbf1ed208f05a0c6c47d912808d6
Spencer Oberstadt <soberstadt@gmail.com>
2021-09-28 17:23:12 -0400

    add types for Head component

---
+++ app/javascript/types/global.d.ts
@@ -0,0 +1,16 @@
+import React from "react"
+
+/**
+ * InertiaJS is missing type definitions for the Head component.
+ * Here we define our own types for the component.
+ *
+ * @see https://github.com/inertiajs/inertia/issues/824
+ * @see https://github.com/inertiajs/inertia/pull/855
+ */
+declare module "@inertiajs/inertia-react" {
+  interface HeadProps {
+    title?: string
+  }
+
+  let Head: React.FC<HeadProps>
+}
