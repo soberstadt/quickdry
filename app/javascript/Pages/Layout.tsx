76784be6600ce81fea2919020763d92471b9d282
Spencer Oberstadt <soberstadt@gmail.com>
2021-09-29 23:25:37 -0400

    max width

--- app/javascript/Pages/Layout.tsx
+++ app/javascript/Pages/Layout.tsx
@@ -19,10 +19,12 @@ export default function Layout({ children, notes }) {
   ));
   return (
     <main className="h-full flex flex-col bg-gray-700 text-gray-50">
-      <header className="h-12 p-3 border-b-2 border-solid border-gray-600">
-        <span>QuickDry</span>
+      <header className="border-b-2 border-solid border-gray-600">
+        <div className="w-full max-w-screen-xl mx-auto p-2">
+          <span className="font-bold font-mono">QuickDry</span>
+        </div>
       </header>
-      <div className="flex flex-grow">
+      <div className="flex flex-grow w-full max-w-screen-xl mx-auto">
         <aside className="flex-shrink-0 w-48 p-2 pt-0">
           <div className="grid divide-y divide-gray-500">{notesList}</div>
         </aside>
