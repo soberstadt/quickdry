ef1e2924c523bb0b99cafcf60af65709980e2fc6
Spencer Oberstadt <soberstadt@gmail.com>
2021-10-16 15:04:53 -0400

    move notes list to component

---
+++ app/javascript/Components/NotesList.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { Link } from "@inertiajs/inertia-react";
+import { Inertia } from "@inertiajs/inertia";
+import { NotePageProps } from "../Pages/Note";
+
+export default function NotesList({ note, notes }: NotePageProps) {
+  const MINUTE_MS = 60000;
+
+  const reloadNotes = () => {
+    Inertia.reload({ only: ["notes"] });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(reloadNotes, MINUTE_MS * 5);
+
+    // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    return () => clearInterval(interval);
+  }, []);
+
+  const notesLink = (note) => {
+    if (note.id == null) {
+      return "/notes/today";
+    } else {
+      return `/notes/${note.id}`;
+    }
+  };
+
+  const notesList = notes.map((iNote) => (
+    <Link
+      key={iNote.id}
+      href={notesLink(iNote)}
+      className="block p-2 rounded-lg
+      bg-gray-400 bg-opacity-20
+      hover:bg-gray-600 hover:bg-opacity-70 hover:text-gray-50
+      dark:bg-gray-800 dark:bg-opacity-20
+      dark:hover:bg-gray-300 dark:hover:bg-opacity-90 dark:hover:text-gray-700"
+    >
+      {note.id == iNote.id ? "> " : ""}
+      {iNote.date_string}
+    </Link>
+  ));
+
+  return <div className="grid gap-2">{notesList}</div>;
+}
--- app/javascript/Pages/Note.tsx
+++ app/javascript/Pages/Note.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "./Layout";
-import { Head, Link } from "@inertiajs/inertia-react";
+import { Head } from "@inertiajs/inertia-react";
 import { Inertia } from "@inertiajs/inertia";
 import { useHotkeys } from "react-hotkeys-hook";
+import NotesList from "../Components/NotesList";
 
 type NoteProp = {
   id: number;
@@ -10,25 +11,12 @@ type NoteProp = {
   date_string: string;
   body: string;
 };
-declare interface NotePageProps {
+export declare interface NotePageProps {
   note: NoteProp;
   notes: Array<NoteProp>;
 }
 
 export default function Note({ note, notes }: NotePageProps) {
-  const MINUTE_MS = 60000;
-
-  const reloadNotes = () => {
-    Inertia.reload({ only: ["notes"] });
-  };
-
-  useEffect(() => {
-    const interval = setInterval(reloadNotes, MINUTE_MS * 5);
-
-    // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
-    return () => clearInterval(interval);
-  }, []);
-
   useHotkeys("cmd+k", () => Inertia.visit("/search"), {
     enableOnTags: ["TEXTAREA"],
   });
@@ -43,34 +31,12 @@ export default function Note({ note, notes }: NotePageProps) {
     }
   };
 
-  const notesLink = (note) => {
-    if (note.id == null) {
-      return "/notes/today";
-    } else {
-      return `/notes/${note.id}`;
-    }
-  };
-
-  const notesList = notes.map((iNote) => (
-    <Link
-      href={notesLink(iNote)}
-      className="block p-2 rounded-lg
-      bg-gray-400 bg-opacity-20
-      hover:bg-gray-600 hover:bg-opacity-70 hover:text-gray-50
-      dark:bg-gray-800 dark:bg-opacity-20
-      dark:hover:bg-gray-300 dark:hover:bg-opacity-90 dark:hover:text-gray-700"
-    >
-      {note.id == iNote.id ? "> " : ""}
-      {iNote.date_string}
-    </Link>
-  ));
-
   return (
     <Layout>
       <Head title={`QuickDry - ${note.date_string}`} />
 
       <aside className="flex-shrink-0 w-48 p-2">
-        <div className="grid gap-2">{notesList}</div>
+        <NotesList note={note} notes={notes}></NotesList>
       </aside>
 
       <article className="flex flex-col flex-grow">
